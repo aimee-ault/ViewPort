7d031c3c1cd1480906654d9ac4661922c28922db
Aimee Ault <aimeeault@gmail.com>
2011-05-24 11:05:57 -0700

    Callback is its own param, not an option

--- jquery.viewport.js
+++ jquery.viewport.js
@@ -4,13 +4,13 @@
 */
 
 (function ($) {
-    $.fn.viewPort = function (options) {
+    $.fn.viewPort = function (options, callback) {
         var  $nodes = $(this)
             ,$win = $(window);
         
         options = $.extend({}, $.fn.viewPort.defaults, options);
         
-        $nodes.live('inViewPort', options.callback);
+        $nodes.live('inViewPort', callback);
         
         function inViewPort() {
             var  winHeight = $win.height()
@@ -41,9 +41,6 @@
     $.fn.viewPort.defaults = {
          buffer: 0  //amount of extra space around viewport (use to allow smoother transitions from hidden to not-hidden)
         ,delimitRate: 0 //rate, in milliseconds to throttle scroll event
-        ,callback: function () {
-            return;
-        }
     };
 
 })(jQuery);
